ba2f838c7ff93cfc45daac39030387f326d8f489
zenitsuza <tuandwcango@gmail.com>
2026-06-10 10:25:28 +0700

    Remove all tabs except ESP and Config - Clean up example

--- Mentality/Example.lua
+++ Mentality/Example.lua
@@ -9,17 +9,7 @@ local Window = Library:Window({
 
 local KeybindList = Library:KeybindList("keys loll fucker")
 
-Window:Category("Enviroment & Transport")
-local CarPage = Window:Page({Name = "Car", Icon = "138827881557940"})
 local ESPPage = Window:Page({Name = "ESP", Icon = "100050851789190", Columns = 1})
-local LanguagePage = Window:Page({Name = "Lang", Icon = "126497581491926"})
-Window:Category("Players & Vision")
-local PlayersPage = Window:Page({Name = "Page", Icon = "134236649319095"})
-local RadarPage = Window:Page({Name = "Radar", Icon = "123554105934637"})
-local WorldPage = Window:Page({Name = "World", Icon = "123944728972740"})
-Window:Category("Utilities & Settings")
-local MiscPage = Window:Page({Name = "Misc", Icon = "103180437044643"})
-local ExploitsPage = Window:Page({Name = "Exploits", Icon = "108839695397679"})
 local SettingsPage = Library:CreateSettingsPage(Window, KeybindList)
 
 local GlobalChat = ESPPage:GlobalChat(1)
@@ -47,114 +37,6 @@ GlobalChat:OnMessageSendPressed(function()
 
     GlobalChat:SendMessage(Avatar, Name, GlobalChat:GetTypedMessage(), Status)
 end)
-
-for Index = 1, 2 do 
-    local Section = CarPage:Section({Name = "Automobiles "..Index, Description = "Exploits for automobiles", Icon = "123944728972740", Side = Index})
-
-    local Toggle = Section:Toggle({
-        Name = "Speed",
-        Flag = "Speed",
-        Default = false,
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    local ToggleSettings = Toggle:Settings(300) -- size
-
-    for i = 1, 15 do 
-        ToggleSettings:Slider({
-            Name = "Speed",
-            Flag = "Speed"..i,
-            Min = 1,
-            Suffix = "km/h",
-            Max = 100,
-            Default = 0,
-            Decimals = 1,
-            Callback = function(Value)
-                print(Value)
-            end
-        })
-
-        ToggleSettings:Label("i cant feel my face"):Colorpicker({
-            Name = "Color",
-            Flag = "Color",
-            Default = Color3.fromRGB(255, 255, 255),
-            Callback = function(Value)
-                print(Value)
-            end
-        })
-    end
-
-    Section:Slider({
-        Name = "Speed",
-        Flag = "Speed",
-        Min = 1,
-        Suffix = "km/h",
-        Max = 100,
-        Default = 0,
-        Decimals = 1,
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    Section:Dropdown({
-        Name = "Dropdown",
-        Flag = "Dropdown",
-        Default = {"First"},
-        Items = {"First", "Second", "Third", "Fourth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"},
-        Multi = true,
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    Section:Label("this a label boi")
-    Section:Label("i cant feel my face"):Colorpicker({
-        Name = "Color",
-        Flag = "Color",
-        Default = Color3.fromRGB(255, 255, 255),
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    Section:Keybind({
-        Name = "triple t",
-        Flag = "Keybind",
-        Default = Enum.KeyCode.X,
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    Section:Textbox({
-        Flag = "Text",
-        Default = "Text",
-        Numeric = false,
-        Placeholder = "...",
-        Finished = true,
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    local b = Section:Listbox({
-        Flag = "Listbox",
-        Default = "First",
-        Size = 275,
-        Items = {"First", "Second", "Third", "Fourth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"},
-        Multi = false,
-        Callback = function(Value)
-            print(Value)
-        end
-    })
-
-    for i = 1, 50 do 
-        b:Add(i)
-    end
-end
 
 Library:Notification({
     Title = "Notification title",
